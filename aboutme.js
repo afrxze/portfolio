aadb7792eaf876f61d01003ec2d3baf223c82160
facebook <facebook@facebooks-MacBook-Pro.local>
2017-06-30 10:03:14 -0700

    pls work

---
+++ aboutme.js
@@ -0,0 +1,24 @@
+h1 {
+	font-family: georgia;
+	color: white;
+}
+h2 {
+	font-family: georgia;
+	color:white ;
+}
+h3{
+	font-family: georgia;
+	color:white ;
+}
+h4{
+	font-family: georgia;
+	color:white ;
+}
+p{
+	font-family: georgia;
+	color: white;
+}
+td{
+	font-family: georgia;
+	color: white;
+}
